c1cc273cbc64fa0d83b4ba1a6b288359dd1b38d2
Mauri <107172051+zothifX@users.noreply.github.com>
2024-05-17 02:44:11 -0400

    Update jcrop.js

--- js/jcrop.js
+++ js/jcrop.js
@@ -15,14 +15,8 @@ $(document).ready(function() {
                 var width = img.width;
                 var height = img.height;
                 if (width > 256 || height > 256) {
-                    var ratio = width / height;
-                    if (ratio > 1) {
                         width = 256;
-                        height = Math.round(width / ratio);
-                    } else {
                         height = 256;
-                        width = Math.round(height * ratio);
-                    }
                 }
                 canvas = document.createElement('canvas');
                 canvas.width = width;
