eab677462e30a43ee40103b9c8dedb9b7bdb451e
Mauri <107172051+zothifX@users.noreply.github.com>
2024-05-17 02:38:17 -0400

    Update jcrop.js

--- js/jcrop.js
+++ js/jcrop.js
@@ -14,13 +14,13 @@ $(document).ready(function() {
             img.onload = function() {
                 var width = img.width;
                 var height = img.height;
-                if (width > 250 || height > 250) {
+                if (width > 256 || height > 256) {
                     var ratio = width / height;
                     if (ratio > 1) {
-                        width = 250;
+                        width = 256;
                         height = Math.round(width / ratio);
                     } else {
-                        height = 250;
+                        height = 256;
                         width = Math.round(height * ratio);
                     }
                 }
